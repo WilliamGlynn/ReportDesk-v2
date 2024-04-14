c5f44a1d0d6c33a9f68588a7b75c162fba731fc5
WilliamGlynn <ryuwill@sbcglobal.net>
2024-04-14 18:59:00 -0500

    Report Protection

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express';
-
 import { daily_report, monthly_report, yearly_report, location_report, duration_report, courses_report, test, head_count, insert_question } from '../controllers/reportControl.js'
 export const routerReport = express.Router()
-
-routerReport.get('/getDaily', daily_report)
-routerReport.get('/getMonthly', monthly_report)
-routerReport.get('/getYearly', yearly_report)
-routerReport.get('/getLocation', location_report)
-routerReport.get('/getDuration', duration_report)
-routerReport.get('/getCourses', courses_report)
-routerReport.get('/test', test)
-routerReport.post('/insertHeadcount', head_count)
-routerReport.post('/insert-question', insert_question);
+import { isAuthenticated } from '../middleware/authMiddleware.js';
+import{checkPermission} from '../middleware/rbacMiddleware.js';
+routerReport.get('/getDaily', isAuthenticated,checkPermission("reports"), daily_report)
+routerReport.get('/getMonthly', isAuthenticated, checkPermission("reports"), monthly_report)
+routerReport.get('/getYearly', isAuthenticated,checkPermission("reports"), yearly_report)
+routerReport.get('/getLocation', isAuthenticated,checkPermission("reports"),  location_report)
+routerReport.get('/getDuration', isAuthenticated,checkPermission("reports"),  duration_report)
+routerReport.get('/getCourses', isAuthenticated,checkPermission("reports"),  courses_report)
+routerReport.post('/insertHeadcount', isAuthenticated,checkPermission("reports"),  head_count)
+routerReport.post('/insert-question', isAuthenticated,checkPermission("reports"),  insert_question);
